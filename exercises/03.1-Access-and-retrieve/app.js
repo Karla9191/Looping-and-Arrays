a15ead7ae615d9e22d1abda0d28e4a047a7d4559
Karla9191 <marietha2014@gmail.com>
2021-02-18 02:24:29 +0000

    NuevoInicio

--- exercises/03.1-Access-and-retrieve/app.js
+++ exercises/03.1-Access-and-retrieve/app.js
@@ -3,6 +3,21 @@ var myArray = ['sunday','monday','tuesday','wednesday','thursday','friday','satu
 
 //1. print the item here
 
+console.log(myArray);
+
 //2. change 'thursday'a value here to null
 
+
+let a=0;
+while(a = myArray.length[4]){
+
+    
+
+
+}
+
 //3. print the position of step 2
+
+
+
+
